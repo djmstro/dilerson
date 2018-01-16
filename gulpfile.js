d1d1e862b8bf1b606ae621964af48ab5c2eb428a
djmstro <djmstro@mail.ru>
2018-01-16 13:36:24 +0300

    add some js files

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var gulp       = require('gulp'), // Подключаем Gulp
     imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
     pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
     cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
-    rigger 			 = require('gulp-rigger'), // Подключаем библиотеку для объединения файлов в один
+    rigger 		 = require('gulp-rigger'), // Подключаем библиотеку для объединения файлов в один
     autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
 gulp.task('sass', function(){ // Создаем таск Sass
@@ -33,7 +33,8 @@ gulp.task('scripts', function() {
     return gulp.src([ // Берем все необходимые библиотеки
         'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
         'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js', // Берем Magnific Popup
-        'app/libs/tooltips.js', // Берем Magnific Popup
+        'app/libs/tooltips.js',
+        'app/libs/popover.js',
         'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
         'node_modules/bootstrap/js/dist/util.js',
         'node_modules/bootstrap/js/dist/button.js',
